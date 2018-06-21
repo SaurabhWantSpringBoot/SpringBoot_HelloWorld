67c1c59daa7ddcfbbc48a7f56ecba585b0ca12e8
Saurabh <saurabh@winzogames.com>
2018-06-22 01:23:59 +0530

    Spring Boot Hello world First commit

---
+++ BootHello/src/main/java/com/boot/controller/BootController.java
@@ -0,0 +1,17 @@
+package com.boot.controller;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@ComponentScan
+public class BootController {
+	
+	@GetMapping("/welcome")
+	public String welcomeRequest()
+	{
+		return "Saurabh";
+	}
+
+}
